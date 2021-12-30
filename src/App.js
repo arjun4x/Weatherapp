00b56a7b4b9ad6ee3ad2cbf3e566c651108e0b1b
Arjun c s <arjunsadhasivan@gmail.com>
2021-12-29 23:44:08 -0800

    weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, {useState}from 'react';
 import './App.css';
 
+const api ={
+  api : "a200ea93f7d60cf66f748f5c3fd8be01" ,
+  base :"http://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
+
+  const [query,setQuery] = useState('');
+  const [weather,setWeather] = useState({});
+
+  const search = (evt) =>{
+    if(evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res=>res.json())
+      .then(result=>{
+        setQuery('');
+        setWeather(result);
+        console.log(result);
+      });
+    }
+  }
+  const dateBuilder = (d) =>{
+    let months =["Januvary","February","March","April","May","june","july",
+    "August","September","October","November","December"];
+    let days =["Sunday","Monday","Tuesday","Wednesday",
+    "Thurday","Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <main>
+      <div className="search">
+        <input type="text"
+        className="search-bar"
+        placeholder="Search...."
+        onChange={e=>setQuery(e.target.value)}
+        value={query}
+        onKeyPress={search}/>
+      </div>
+      <div className="location-box">
+        <div className="location">Kochi , kerala</div>
+        <div className="date">{dateBuilder(new Date())}</div>
+      </div>
+      <div className="weather-box">
+        <div className="temp">
+        15°C
+        </div>
+        </div>
+        <div className="weather1-box">
+          Sunny
+        </div>
+      
+      </main>
+      
+       
     </div>
   );
 }
